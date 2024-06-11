af48344f4fd1f909fdb2e5ac63fb1559aa07059a
gabipirillo <gpmanoel123@gmail.com>
2024-06-10 23:04:58 -0300

    html+css

---
+++ js/script.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('form');
+  
+    form.addEventListener('submit', (e) => {
+      const documento = document.getElementById('documento').value;
+      const paginas = document.getElementById('paginas').value;
+      const tempo = document.getElementById('tempo').value;
+      const imagens = document.getElementById('imagens').value;
+  
+      const dados = {
+        documento,
+        paginas,
+        tempo,
+        imagens
+      };
+  
+      // Armazena os dados no localStorage
+      localStorage.setItem('formulario', JSON.stringify(dados));
+  
+    //   return false; // impede que o formulário seja submetido ao servidor
+    });
+  });
